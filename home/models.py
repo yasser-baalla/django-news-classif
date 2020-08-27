0ac8bf9be29115fc050f7ae50ac859a136b001da
yasser-baalla <yasserbaalla@gmail.com>
2020-08-27 15:41:55 +0100

    models created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+
+
+class category(models.Model):
+    url = models.CharField()
+    id_cat = models.Charfield(primary_key=True)
+    pos_neg = models.Charfield()
+    simp_comp = models.Charfield()
+    title = models.Charfield()
+    image = models.Charfield()
+
+
+class category_info(models.Model):
+    id_cat = models.ForeignKey(category, on_delete=models.CASCADE)
+    politique = models.BooleanField()
+    parties_politiques = models.BooleanField()
+    parlement = models.BooleanField()
+    Histoire = models.BooleanField()
+    economie = models.BooleanField()
+    tourisme = models.BooleanField()
+    bourse = models.BooleanField()
+    immobilier = models.BooleanField()
+    sport = models.BooleanField() 
+    football = models.BooleanField()
+    société = models.BooleanField() 
+    réseaux_sociaux = models.BooleanField()
+    santé = models.BooleanField() 
+    covid = models.BooleanField()
+    science = models.BooleanField() 
+    agriculture = models.BooleanField() 
+    espace = models.BooleanField()
+    nature = models.BooleanField() 
+    animaux = models.BooleanField()
+    religion = models.BooleanField()
+    eid_el_adha = models.BooleanField()
+    revue = models.BooleanField() 
+    revue_presse = models.BooleanField() 
+    revue_web = models.BooleanField()
+    rumeurs = models.BooleanField()
+    statistiques = models.BooleanField()
+    célébrité = models.BooleanField()
+    divertissement = models.BooleanField()
+    monde = models.BooleanField()
+    culture = models.BooleanField()
+    Local Trends = models.BooleanField()
+    loi_décret = models.BooleanField()
+    terrorisme = models.BooleanField()
+    meteo = models.BooleanField()
+    blessures = models.BooleanField()
+    education = models.BooleanField()
